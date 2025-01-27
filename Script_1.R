1f6a9fd391db6bfb0136193f30823c530baa52f7
Marlen_PC <Marlen_gtz96@hotmail.com>
2025-01-27 13:27:37 -0600

    Scrip 1 de prueba potencial de R

---
+++ Script_1.R
@@ -0,0 +1,31 @@
+# Marlen Gutierrez Barrientos
+# 27/01/2025
+# Matr: 1595194
+#
+
+
+# Datos básicos -----------------------------------------------------------
+
+3 + 5
+8 * 4
+3 / 2
+
+suma <- 3 + 5 # se le asigna una operacion a la palabra suma 
+suma 
+suma * 4
+suma * suma # si funciona por usar la forma correcta el nombre
+
+suma * Suma # forma incorrecta, no existe Suma (s)
+
+
+# Importar datos ----------------------------------------------------------
+
+edad <- c(50, 25, 30, 18, 45, 25, 36, 17, 45, 24,
+            22, 18, 41, 43) # la letra (c) es un comando para concatenar los datos es importante para agrupar los datos siempre la debes poner
+edad
+mean(edad)
+var(edad)
+sd(edad)
+
+edad * 2
+edad * c(2,3)
